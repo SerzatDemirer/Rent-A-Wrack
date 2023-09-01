61a09eeb69ceb4a5c1ed570f1861a2c4b4afcb7d
Serzat Demirer <91182016+SerzatDemirer@users.noreply.github.com>
2023-09-01 13:28:54 +0200

    bug fixed

--- Rent-A-Wreck/Program.cs
+++ Rent-A-Wreck/Program.cs
@@ -1,8 +1,9 @@
-﻿using  static System.Console;
+﻿using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-
+using Newtonsoft.Json;
+using System.IO;
 
 public class Vehicle
 {
@@ -11,7 +12,6 @@ public class Vehicle
     public string Type { get; set; }
     public string RegistrationNumber { get; set; }
 
-
     public Vehicle(string brand, string model, string type, string registrationNumber)
     {
         Brand = brand;
@@ -19,44 +19,33 @@ public class Vehicle
         Type = type;
         RegistrationNumber = registrationNumber;
     }
-
 }
 
 public class Program
 {
-    private static List<Vehicle> cars = new List<Vehicle>();
+    private static List<Vehicle> Cars = new List<Vehicle>();
 
     public static void Main()
     {
+        LoadCarsFromFile();
         while (true)
         {
             ShowMainMenu();
-            
         }
     }
 
-    public static void ShowMainMenu()
+    private static void ShowMainMenu()
     {
-        Clear();
-        ForegroundColor = ConsoleColor.DarkRed;
-        WriteCentered("********** Rent - A - Wreck * *********");
-        ForegroundColor = ConsoleColor.DarkYellow;
-        WriteCentered("1. Registrera fordon");
-        WriteCentered("2. Lista fordon");
-        WriteCentered("3. Sök fordon");
-        WriteCentered("4. Avsluta");
-        ResetColor();
+        ClearScreenWithColors(ConsoleColor.DarkRed);
+        DisplayMenuOptions();
 
-        //var choice = Console.ReadLine();
-
-        switch (Console.ReadLine())
+        switch (ReadLine())
         {
             case "1":
                 RegisterCar();
                 break;
-
             case "2":
-                ListCar();
+                ListCars();
                 break;
             case "3":
                 SearchCar();
@@ -64,89 +53,165 @@ public class Program
             case "4":
                 ExitProgram();
                 break;
-            
-
+            default:
+                WriteCentered("Välj ett giltigt alternativ!");
+                break;
         }
     }
 
+    private static void ClearScreenWithColors(ConsoleColor textColor)
+    {
+        Clear();
+        ForegroundColor = textColor;
+    }
+
+    private static void AddSpaceLines(int numberOfLines)
+    {
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            WriteLine();
+        }
+    }
+
+    private static void DisplayMenuOptions()
+    {
+        WriteLine("=================================================================================================================================");
+        WriteCentered("********** Rent - A - Wreck **********");
+        WriteLine("=================================================================================================================================");
+
+        AddSpaceLines(2);
+        ForegroundColor = ConsoleColor.DarkYellow;
+
+        int leftPadding = (WindowWidth / 2) - 20;
+
+        WritePadded("1. Registrera fordon", leftPadding);
+        AddSpaceLines(1);
+        WritePadded("2. Lista fordon", leftPadding);
+        AddSpaceLines(1);
+        WritePadded("3. Sök fordon", leftPadding);
+        AddSpaceLines(1);
+        WritePadded("4. Avsluta", leftPadding);
+        AddSpaceLines(1);
+
+        ResetColor();
+    }
+
+    private static void WritePadded(string message, int leftPadding)
+    {
+        WriteLine(message.PadLeft(message.Length + leftPadding));
+    }
 
     private static void WriteCentered(string message)
     {
         int screenWidth = WindowWidth;
         int stringWidth = message.Length;
         int spaces = (screenWidth / 2) - (stringWidth / 2);
-
         WriteLine(message.PadLeft(message.Length + spaces));
     }
 
-    static void RegisterCar()
+    private static void RegisterCar()
     {
         Clear();
         ForegroundColor = ConsoleColor.Cyan;
-        WriteCentered("Märke: ");
-        var brand = ReadLine();
-        WriteCentered("Model: ");
-        var model = ReadLine();
-        WriteCentered("Typ: ");
-        var type = ReadLine();
-        WriteCentered("Registreringnummer: ");
-        var registreringNummer = ReadLine();
 
+        var brand = GetUserInput("Märke:");
+        var model = GetUserInput("Model:");
+        var type = GetUserInput("Typ:");
+        var registrationNumber = GetUserInput("Registreringnummer:");
 
-        var vehicle = new Vehicle(brand, model, type, registreringNummer);
-        cars.Add(vehicle);
+        var vehicle = new Vehicle(brand, model, type, registrationNumber);
+        Cars.Add(vehicle);
+        SaveCarsToFile();
 
         WriteCentered("Fordonet är registrerat!");
-        Thread.Sleep(2000);
-
+        PauseBeforeContinuing();
     }
 
-    static void ListCar()
+    private static string GetUserInput(string prompt)
     {
-       Clear();
-       WriteCentered("Märke    Modell      Typ     RegistreringsNummer");
-       WriteCentered("=================================================");
+        int screenWidth = WindowWidth;
+        int stringWidth = prompt.Length;
+        int spaces = (screenWidth / 2) - (stringWidth / 2);
+        Write(prompt.PadLeft(prompt.Length + spaces));
+        return ReadLine() ?? string.Empty;
+    }
 
-        foreach (var vehicle in cars)
+    private static void PauseBeforeContinuing()
+    {
+        Thread.Sleep(2000);
+    }
+
+    private static void ListCars()
+    {
+        ClearScreenWithColors(ConsoleColor.DarkYellow);
+        WriteCentered("            Märke               Modell               Typ                RegistreringsNummer");
+        WriteCentered("============================================================================================");
+
+        foreach (var vehicle in Cars)
         {
-            WriteCentered($"{vehicle.Brand.PadRight(10)}     {vehicle.Model.PadRight(10)}     {vehicle.Type.PadRight(10)}      {vehicle.RegistrationNumber}");
+            WriteCentered($"{vehicle.Brand.PadRight(20)}{vehicle.Model.PadRight(20)}{vehicle.Type.PadRight(20)}{vehicle.RegistrationNumber}");
         }
-
         ReadLine();
     }
 
-    static void SearchCar()
+    private static void SearchCar()
     {
         Clear();
-        WriteCentered("Sök registreringsnummer!");
-        var registreringsNummer = ReadLine();
-        var carFound = cars.Find(vehicle =>
-            string.Equals(vehicle.RegistrationNumber, registreringsNummer, StringComparison.OrdinalIgnoreCase));
+        var registrationNumber = GetUserInput("Sök registreringsnummer:");
+        var carFound = Cars.Find(vehicle => string.Equals(vehicle.RegistrationNumber, registrationNumber, StringComparison.OrdinalIgnoreCase));
 
-        if (carFound != null)
+        DisplaySearchResults(carFound);
+        ReadLine();
+    }
+
+    private static void DisplaySearchResults(Vehicle? car)
+    {
+        if (car != null)
         {
-            WriteCentered($"Merke {carFound.Brand}");
-            WriteCentered($"Modell {carFound.Model}");
-            WriteCentered($"Typ {carFound.Type}");
-            WriteCentered($"Registreringsnummer {carFound.RegistrationNumber}");
+            DisplayVehicleDetails(car);
         }
         else
         {
             WriteCentered("Fordon hittades inte!");
-
         }
-        ReadLine();
     }
 
+    private static void DisplayVehicleDetails(Vehicle car)
+    {
+        WriteLine($"Merke: {car.Brand}");
+        AddSpaceLines(1);
 
-    static void ExitProgram()
+        WriteLine($"Modell: {car.Model}");
+        AddSpaceLines(1);
+
+        WriteLine($"Typ: {car.Type}");
+        AddSpaceLines(1);
+
+        WriteLine($"Registreringsnummer: {car.RegistrationNumber}");
+    }
+
+    private const string StorageFile = "cars.json";
+
+    private static void SaveCarsToFile()
+    {
+        var jsonData = JsonConvert.SerializeObject(Cars);
+        File.WriteAllText(StorageFile, jsonData);
+    }
+
+    private static void LoadCarsFromFile()
+    {
+        if (File.Exists(StorageFile))
+        {
+            var jsonData = File.ReadAllText(StorageFile);
+            Cars = JsonConvert.DeserializeObject<List<Vehicle>>(jsonData);
+        }
+    }
+
+    private static void ExitProgram()
     {
         Clear();
         WriteCentered("Rent-A-Wreck avslutas nu!");
-        Thread.Sleep(3000);
-        Environment.Exit(0);       
-
+        PauseBeforeContinuing();
+        Environment.Exit(0);
     }
-
 }
-
